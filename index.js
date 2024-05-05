ae7552a951dbbdcc859a3f1eda9c2bce836a4848
spccsdpg <rautajju2500@gmail.com>
2024-05-05 12:06:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const server = http.createServer((req, res) => {
   res.end('Hello, World!');
 });
 
-server.listen(5000, '0.0.0.0');
+server.listen(3000, '0.0.0.0');
 
 console.log('Server listening on port 3000');
